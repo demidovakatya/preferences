ac10ea1d31884e0e354a1101299fdd3c6ef4f3ad
Katya Demidova <demidovakatya@gmail.com>
2016-05-26 22:01:07 +0300

    Add some stuff for my work computer

---
+++ work_profile.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install aptitude
+sudo apt-get install gdebi-core
+
+# install google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi google-chrome-stable_current_amd64.deb
+
+sudo apt-get install libreoffice
+
+wget http://download.skype.com/linux/skype-ubuntu-precise_4.2.0.13-1_i386.deb
+sudo gdebi skype-ubuntu-precise_4.2.0.13-1_i386.deb
+
+sudo gdebi dropbox_1.6.0_amd64.deb
