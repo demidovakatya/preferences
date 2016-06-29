7e1c670eaf80b2336491f3959227ae4a6091a746
Katya Demidova <demidovakatya@gmail.com>
2016-06-29 05:01:25 +0300

    add spotify

--- work_profile.sh
+++ work_profile.sh
@@ -58,3 +58,9 @@ sudo apt install ambiance-colors radiance-colors xfce4-dockbarx-plugin dockbarx-
 sudo apt-get install apache2 mysql-server php5 libapache2-mod-php5 filezilla
 sudo /etc/init.d/apache2 restart
 php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
+
+# Spotify
+sudo apt-key adv —keyserver hkp://keyserver.ubuntu.com:80 —recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
