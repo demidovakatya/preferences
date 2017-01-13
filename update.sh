b454700f168ebc757bb0c8d0e2c769c24f068e81
Katya Demidova <demidovakatya@gmail.com>
2017-01-13 05:42:16 +0300

    removed crap

--- update.sh
+++
@@ -1,9 +0,0 @@
-# One command to update them all.
-
-function update
-    set $brew brew update; brew upgrade;
-    set $pip2 pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip2 install -U -q;
-    set $pip3 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip3 install -U -q;
-    sh -c $brew;
-    sudo sh -c $pip2$pip3
-    end
